238833ca112654a3682a0c3a85a8e5268182bb8f
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2017-10-22 16:06:07 +0900

    Add time fucntion, however it's too short for human hand

--- ch1/ex1.3/main.go
+++ ch1/ex1.3/main.go
@@ -4,33 +4,41 @@ import (
 	"os"
 	"fmt"
 	"strings"
+	"time"
 )
 
 func echo1(argv []string){
+	start := time.Now()
 	s, sep := "", ""
 	for i := 1; i < len(argv); i++ {
 		s += sep + argv[i]
 		sep = " "
 	}
 	fmt.Println(s)
+	fmt.Printf("echo1 %.2fs elapsed\n", time.Since(start).Seconds())
 }
 
 func echo2(argv []string){
+	start := time.Now()
 	s, sep := "", ""
 	for _, arg := range argv[1:] {
 		s += sep + arg
 		sep = " "
 	}
 	fmt.Println(s)
+	fmt.Printf("echo2 %.2fs elapsed\n", time.Since(start).Seconds())
 }
 
 func echo3(argv []string){
+	start := time.Now()
 	fmt.Println(strings.Join(argv[1:], " "))
-}
+	fmt.Printf("echo3 %.2fs elapsed\n", time.Since(start).Seconds())
+	}
 
 
 func  main()  {
 	echo1(os.Args)
 	echo2(os.Args)
 	echo3(os.Args)
+
 }
