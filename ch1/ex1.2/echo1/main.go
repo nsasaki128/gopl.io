990faeeb6d1be4d7a3b8b5f793126b4c2100b6b7
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2017-10-23 22:16:52 +0900

    Forget adding ex1.2 echo1

--- ch1/ex1.2/echo1/main.go
+++ ch1/ex1.2/echo1/main.go
@@ -4,13 +4,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main(){
 	var s, sep string
+	sep = " "
 	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = " "
+		s = strconv.Itoa(i) + sep + os.Args[i]
+		fmt.Println(s)
 	}
-	fmt.Println(s)
 }
