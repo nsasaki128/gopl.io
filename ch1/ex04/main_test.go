e08cafa65c076edf4cc2b35526c186be0f3ae4ea
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2017-11-02 00:57:56 +0900

    Add output test case

--- ch1/ex04/main_test.go
+++ ch1/ex04/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"bytes"
 )
 
 
@@ -81,3 +82,56 @@ func TestCountLine(t *testing.T) {
 
 }
 
+
+func TestWriteCounts(t *testing.T){
+	var tests = []struct{
+		caseName string
+		inputResults map[string]map[string]int
+		expected string
+	}{
+		{
+			caseName : string("1 file and 1 line"),
+			inputResults : map[string]map[string]int{"a" : {"sample.txt" : 1}},
+			expected : string(""),
+		},
+		{
+			caseName: string("1 file and 2 same line"),
+			inputResults : map[string]map[string]int{"a" : {"sample.txt" : 2}},
+			expected : string("2\ta\tsample.txt\n"),
+		},
+		{
+			caseName : string("1 file and 2 different line"),
+			inputResults : map[string]map[string]int{"a" : {"sample.txt" : 1}, "b" : {"sample.txt" : 1}},
+			expected : string(""),
+		},
+		{
+			caseName : string("2 file and same line"),
+			inputResults : map[string]map[string]int{"a" : {"sample.txt" : 1, "sample2.txt" : 1}},
+			expected : string("2\ta\tsample.txt\tsample2.txt\n"),
+		},
+		{
+			caseName : string("2 file and different line"),
+			inputResults : map[string]map[string]int{"a" : {"sample.txt" : 1}, "b" : {"sample2.txt" : 1}},
+			expected : string(""),
+		},
+		{
+			caseName : string("combine 2 files same and different"),
+			inputResults : map[string]map[string]int{"a" : {"sample.txt": 1}, "b" : {"sample.txt" : 1, "sample2.txt" : 1}, "c" : {"sample2.txt" : 1}},
+			expected : string("2\tb\tsample.txt\tsample2.txt\n"),
+		},
+		{
+			caseName : string("combine 3 files same and different"),
+			inputResults : map[string]map[string]int{"a" : {"sample.txt": 2}, "b" : {"sample.txt" : 1, "sample2.txt" : 1, "sample3.txt" : 2}, "c" : {"sample2.txt" : 1}},
+			expected : string("2\ta\tsample.txt\n4\tb\tsample.txt\tsample2.txt\tsample3.txt\n"),
+		},
+	}
+
+	for _, test := range tests {
+		out = new(bytes.Buffer)
+		writeCounts(test.inputResults)
+		got := out.(*bytes.Buffer).String()
+		if got != test.expected {
+			t.Errorf("error in case \"%s\" expected results is \"%s\" and actual result is \"%s\".", test.caseName, test.expected, got)
+		}
+	}
+}
