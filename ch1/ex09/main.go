46aab4a5bda5dd545c328d64c161ec0521f5fbb1
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2017-11-05 12:16:41 +0900

    Copy from ex08

---
+++ ch1/ex09/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"io"
+	"strings"
+)
+
+const (
+	urlHeader = "http://"
+)
+
+func main() {
+
+	for _, url := range os.Args[1:] {
+		writeRespBody(url)
+	}
+}
+
+func writeRespBody(url string) {
+
+	url = addUrlHeaderIfNeeded(url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		os.Exit(1)
+	}
+
+	_, err = io.Copy(os.Stdout, resp.Body)
+	if err != nil{
+		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		os.Exit(1)
+	}
+	resp.Body.Close()
+
+}
+
+func addUrlHeaderIfNeeded(url string) string {
+	if !strings.HasPrefix(url, urlHeader) {
+		url = urlHeader + url
+	}
+	return url
+}
