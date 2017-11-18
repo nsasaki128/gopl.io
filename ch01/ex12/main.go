a3b644b20319c7b56d97b78dc2267d46a16f9cec
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2017-11-18 15:44:35 +0900

    Change const to var

--- ch01/ex12/main.go
+++ ch01/ex12/main.go
@@ -36,13 +36,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func lissajous(out io.Writer) {
 	rand.Seed(time.Now().UTC().UnixNano())
-	const (
-		cycles  = 5     // 発信器xが完了する周回の回数
-		res     = 0.001 // 回転の分解能
-		size    = 100   // 画像キャンパスは [-size..+size] の範囲で扱う
-		nframes = 64    // アニメーションフレーム数
-		delay   = 8     // 10ms単位でのフレーム間の遅延
-	)
+		cycles  := 5     // 発信器xが完了する周回の回数
+		res     := 0.001 // 回転の分解能
+		size    := 100   // 画像キャンパスは [-size..+size] の範囲で扱う
+		nframes := 64    // アニメーションフレーム数
+		delay   := 8     // 10ms単位でのフレーム間の遅延
 
 	freq := rand.Float64() * 3.0 // 発信器yの相対周波数
 	anim := gif.GIF{LoopCount: nframes}
@@ -51,10 +49,10 @@ func lissajous(out io.Writer) {
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img  := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), greenIndex)
+			img.SetColorIndex(size+int(x*float64(size)+0.5), size+int(y*float64(size)+0.5), greenIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
