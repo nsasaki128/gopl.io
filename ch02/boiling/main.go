4a9e68bc0ea6a28c1f87dcedbd958b872d0605d7
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2017-11-19 12:40:27 +0900

    Add chapter02 boiling

---
+++ ch02/boiling/main.go
@@ -0,0 +1,14 @@
+//boilingは水の沸騰点を表示します。
+package main
+
+import "fmt"
+
+const boilingF = 212.0
+
+func main()  {
+	var f = boilingF
+	var c = (f - 32) * 5 / 9
+	fmt.Printf("boiling point = %g°F or %g°C\n", f, c)
+	// 出力:
+	// boiling point = 212°F or 100°C
+}
