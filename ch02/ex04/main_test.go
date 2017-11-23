29a88d0edd104720b59b331f44703c3643f6dd29
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2017-11-23 12:43:37 +0900

    Solve problems and add divide and conquer program for fun :)

---
+++ ch02/ex04/main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"testing"
+)
+var testCases = []struct {
+	name     string
+	input    uint64
+	expected int
+}{
+	{name: "input 0", input: 0, expected: 0},
+	{name: "input 1", input: 1, expected: 1},
+	{name: "input 2", input: 2, expected: 1},
+	{name: "input 3", input: 3, expected: 2},
+	{name: "input 4", input: 4, expected: 1},
+	{name: "input MAX", input: 1<<63 - 1, expected: 63},
+}
+
+func TestPopCount(t *testing.T) {
+	for _, testCase := range testCases {
+		result := PopCount(testCase.input)
+		if result != testCase.expected {
+			t.Errorf("case %s expected %s actual %s", testCase.name, testCase.expected, result)
+		}
+	}
+}
+func TestIteratePopCount(t *testing.T) {
+	for _, testCase := range testCases {
+		result := IteratePopCount(testCase.input)
+		if result != testCase.expected {
+			t.Errorf("case %s expected %s actual %s", testCase.name, testCase.expected, result)
+		}
+	}
+}
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount(uint64(i))
+	}
+}
+func BenchmarkIteratePopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IteratePopCount(uint64(i))
+	}
+}
---
+++ ch02/ex05/main.go
@@ -0,0 +1,38 @@
+package main
+
+// pc[i]はiのポピュレーションカウントです。
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCountはxのポピュレーションカウント（1が設定されているビット数）を返します。
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func IteratePopCount(x uint64) int {
+	var out int
+	for i := uint(0); i < 8; i++ {
+		out += int(pc[byte(x>>(i*8))])
+	}
+	return out
+}
+
+func ShiftPopCount(x uint64) int {
+	var out int
+	for i := uint(0); i < 64; i++ {
+		out += int(x>>i)&1
+	}
+	return out
+}
