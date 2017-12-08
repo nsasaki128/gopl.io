ac8fd158b3c97450d9d7a8e8623c41428673eb6c
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2017-12-08 12:24:00 +0900

    Solve using hsv color space

--- ch03/ex05/main.go
+++ ch03/ex05/main.go
@@ -7,6 +7,7 @@ import (
 	"image/png"
 	"math/cmplx"
 	"os"
+	"math"
 )
 
 func main()  {
@@ -26,16 +27,30 @@ func main()  {
 	}
 	png.Encode(os.Stdout, img)
 }
-
+//See http://courses.cms.caltech.edu/cs11/material/java/donnie/lab3/index.html
+//This pattern uses hsv for iteration num
 func mandelbrot(z complex128) color.Color {
-	const iterations = 200
 	const contrast = 15
+	const iterationRange = math.MaxUint8 / contrast
+	const iterations = iterationRange*6
 
 	var v complex128
-	for n := uint8(0); n < iterations; n++ {
+	for n := 0; n < iterations; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
-			return color.Gray{255 - contrast*n}
+			if n < iterationRange {
+				return color.RGBA{255, uint8(contrast*n), 0, 255}
+			} else if n < 2*iterationRange {
+				return color.RGBA{uint8(255-contrast*(n-iterationRange)), 255, 0, 255}
+			} else if n < 3*iterationRange {
+				return color.RGBA{0, 255, uint8(contrast*(n-2*iterationRange)), 255}
+			} else if n < 4*iterationRange {
+				return color.RGBA{0, uint8(255-contrast*(n-3*iterationRange)), 255, 255}
+			} else if n < 5*iterationRange {
+				return color.RGBA{uint8(contrast*(n-4*iterationRange)),0 , 255, 255}
+			} else {
+				return color.RGBA{255, 0, uint8(255-contrast*(n-5*iterationRange)), 255}
+			}
 		}
 	}
 	return color.Black
