0149793f538a1b70f71e54dfd56cc5c4d034e62c
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2017-12-08 10:51:11 +0900

    Add test case

--- ch03/ex04/main_test.go
+++ ch03/ex04/main_test.go
@@ -3,6 +3,9 @@ package main
 import (
 	"testing"
 	"math"
+	"net/http/httptest"
+	"net/http"
+	"fmt"
 )
 
 func TestIsInvalid(t *testing.T) {
@@ -44,3 +47,53 @@ func TestIsInvalid(t *testing.T) {
 	}
 }
 
+func TestCreateSvgInfo(t *testing.T) {
+	testCases := []struct {
+		name     string
+		query    string
+		expected svgInfo
+	}{
+		{name: "no query", query: "", expected: svgInfo{width:600, height:320, cells:100, xyrange:30.0, color:"white", surface:"sinc"}},
+		{name: "only width", query: "width=200", expected: svgInfo{width:200, height:320, cells:100, xyrange:30.0, color:"white", surface:"sinc"}},
+		{name: "only height", query: "height=300", expected: svgInfo{width:600, height:300, cells:100, xyrange:30.0, color:"white", surface:"sinc"}},
+		{name: "only cells", query: "cells=10", expected: svgInfo{width:600, height:320, cells:10, xyrange:30.0, color:"white", surface:"sinc"}},
+		{name: "only xyrange", query: "xyrange=20.0", expected: svgInfo{width:600, height:320, cells:100, xyrange:20.0, color:"white", surface:"sinc"}},
+		{name: "only color", query: "color=black", expected: svgInfo{width:600, height:320, cells:100, xyrange:30.0, color:"black", surface:"sinc"}},
+		{name: "only surface", query: "surface=egg", expected: svgInfo{width:600, height:320, cells:100, xyrange:30.0, color:"white", surface:"egg"}},
+		{name: "miss surface", query: "surface=hoge", expected: svgInfo{width:600, height:320, cells:100, xyrange:30.0, color:"white", surface:"sinc"}},
+		{name: "all", query: "width=200&height=300&cells=10&xyrange=20.0&color=black&surface=saddle", expected: svgInfo{width:200, height:300, cells:10, xyrange:20.0, color:"black", surface:"saddle"}},
+	}
+	//for float error
+	const eps= 1e-10
+	for _, testCase := range testCases {
+		ts := httptest.NewServer(http.HandlerFunc(handler))
+
+		res, _ := http.Get(fmt.Sprintf("%v?%v", ts.URL, testCase.query))
+		res.Request.ParseForm()
+		actual := createSvgInfo(res.Request.Form)
+		if actual.width != testCase.expected.width {
+			t.Errorf("error in case %s width\nexpected:\t%d\nactual:\t%d\n", testCase.name, testCase.expected.width, actual.width)
+			continue
+		}
+		if actual.height != testCase.expected.height {
+			t.Errorf("error in case %s height\nexpected:\t%d\nactual:\t%d\n", testCase.name, testCase.expected.height, actual.height)
+			continue
+		}
+		if actual.cells != testCase.expected.cells {
+			t.Errorf("error in case %s cells\nexpected:\t%d\nactual:\t%d\n", testCase.name, testCase.expected.cells, actual.cells)
+			continue
+		}
+		if math.Abs(actual.xyrange-testCase.expected.xyrange) > eps {
+			t.Errorf("error in case %s xyrange\nexpected:\t%f\nactual:\t%f\n", testCase.name, testCase.expected.xyrange, actual.xyrange)
+			continue
+		}
+		if actual.color != testCase.expected.color {
+			t.Errorf("error in case %s color\nexpected:\t%s\nactual:\t%s\n", testCase.name, testCase.expected.color, actual.color)
+			continue
+		}
+		if actual.surface != testCase.expected.surface {
+			t.Errorf("error in case %s surface\nexpected:\t%s\nactual:\t%s\n", testCase.name, testCase.expected.surface, actual.surface)
+			continue
+		}
+	}
+}
