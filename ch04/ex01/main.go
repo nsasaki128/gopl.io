d4b8177e5477d53a7733cae5ea88af088d9f6977
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2017-12-21 22:43:15 +0900

    Add os.Exit

--- ch04/ex01/main.go
+++ ch04/ex01/main.go
@@ -6,19 +6,19 @@ import (
 	"os"
 )
 
-
-func main()  {
+func main() {
 	if len(os.Args) != 3 {
 		fmt.Fprintf(os.Stderr, "Usage: %s, s1, s2", os.Args[0])
+		os.Exit(1)
 	}
 	dif := difBitCountInSha256([]byte(os.Args[1]), []byte(os.Args[2]))
 	fmt.Fprintf(os.Stdout, "sha256 hash bit count differece between %s and %s is %d\n", os.Args[1], os.Args[2], dif)
 }
 
 func DivideAndConquerPopCount(x uint8) int {
-	x = (x&0x55) + ((x>>1)&0x55)
-	x = (x&0x33) + ((x>>2)&0x33)
-	x = (x&0x0F) + ((x>>4)&0x0F)
+	x = (x & 0x55) + ((x >> 1) & 0x55)
+	x = (x & 0x33) + ((x >> 2) & 0x33)
+	x = (x & 0x0F) + ((x >> 4) & 0x0F)
 	return int(x)
 }
 
