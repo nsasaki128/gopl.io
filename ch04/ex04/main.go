11ce8a66d0c414e484d6c929ffd07ae4ed784e18
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2017-12-26 15:06:53 +0900

    Fix not using copy

--- ch04/ex04/main.go
+++ ch04/ex04/main.go
@@ -2,36 +2,32 @@ package main
 
 import "fmt"
 
-func main()  {
-	input := []int{1,2,3,4,5,6,7,8,9,10,11,12}
+func main() {
+	input := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
 	fmt.Printf("original:\t%v\n", input)
 	for i := 1; i < len(input); i++ {
-		fmt.Printf("rotate %d:\t%v\n", i, rotate(input, i))
+		rotate(input, i)
+		fmt.Printf("rotate %d:\t%v\n", i, input)
 	}
 }
 
-func gcd (x, y int) int {
+func gcd(x, y int) int {
 	for y != 0 {
 		x, y = y, x%y
 	}
 	return x
 }
 
-func rotate(input []int, rotate int) []int {
-	output := make([]int, len(input))
-	copy(output, input)
+func rotate(input []int, rotate int) {
 	n := gcd(len(input), rotate)
 	for i := 0; i < n; i++ {
 		//rotateEach
-		prev := output[i]
+		prev := input[i]
 		for j := 0; j < len(input)/n; j++ {
-			next := (i+(j+1)*rotate)%len(input)
-			tmp := output[next]
-			output[next] = prev
+			next := (i + (j+1)*rotate) % len(input)
+			tmp := input[next]
+			input[next] = prev
 			prev = tmp
 		}
 	}
-	return output
 }
-
-
