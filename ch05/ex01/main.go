d041a2a2249e1e39fa871511bfcb27815995eb45
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2017-12-26 16:19:31 +0900

    Copy from findlinks1

---
+++ ch05/ex01/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+
+	"fmt"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlink1: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+
+}
+
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
+}
