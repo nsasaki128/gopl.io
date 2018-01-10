1e3ff36e52dc095802afb92ff9e0101e74f883cf
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2018-01-10 22:40:17 +0900

    Fix function name

--- ch05/ex08/main_test.go
+++ ch05/ex08/main_test.go
@@ -8,7 +8,7 @@ import (
 	"golang.org/x/net/html"
 )
 
-func TestForEachNode(t *testing.T) {
+func TestElementById(t *testing.T) {
 	testCases := []struct {
 		name          string
 		input         string
