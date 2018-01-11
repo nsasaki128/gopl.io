8a6df09fad30e8832bba7f3eb40ee015b9d20525
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2018-01-12 00:00:48 +0900

    Solve ex12

---
+++ ch05/ex12/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+
+	"os"
+
+	"net/http"
+
+	"golang.org/x/net/html"
+)
+
+//forEachNOdeはnから始まるツリー内部の個々のノードxに対して
+//関数pre(x)とpost(x)を呼び出します。その二つの関数はオプションです。
+//preは子ノードを訪れた前に呼び出され
+//postは子ノードを訪れた後に呼び出されます
+
+func main() {
+	for _, url := range os.Args[1:] {
+		outline(url)
+	}
+}
+func outline(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return err
+	}
+	depth := 0
+	startElement := func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+			depth++
+		}
+	}
+	endElement := func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			depth--
+			fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+		}
+	}
+	forEachNode(doc, startElement, endElement)
+
+	return nil
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
