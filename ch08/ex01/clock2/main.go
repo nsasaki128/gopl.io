656661caba1b1ef5104c4fb8e727caeab908ead5
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2018-02-14 00:31:58 +0900

    Fix clock2 for getting port number

---
+++ ch08/ex01/clock2/main.go
@@ -0,0 +1,39 @@
+package clock2
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+var port = flag.Int("port", 8000, "search port")
+
+func main() {
+	flag.Parse()
+	listener, err := net.Listen("tcp", fmt.Sprintf("localhost%d", *port))
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
