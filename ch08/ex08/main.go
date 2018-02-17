2dfac3f707323f09de862c9848afadb397a8f514
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2018-02-17 19:06:04 +0900

    Solve ex08

--- ch08/ex08/main.go
+++ ch08/ex08/main.go
@@ -19,17 +19,37 @@ func echo(c net.Conn, shout string, delay time.Duration) {
 }
 
 func handleConn(c net.Conn) {
+	listen := make(chan string)
+	abort := make(chan struct{})
+	timer := time.NewTimer(10 * time.Second)
+
 	var wg sync.WaitGroup
-	input := bufio.NewScanner(c)
-	for input.Scan() {
-		wg.Add(1)
-		go func(s string) {
-			echo(c, s, 1*time.Second)
-			wg.Done()
-		}(input.Text())
+	go func() {
+		input := bufio.NewScanner(c)
+		for input.Scan() {
+			listen <- input.Text()
+		}
+		abort <- struct{}{}
+	}()
+	for {
+		select {
+		case text := <-listen:
+			wg.Add(1)
+			go func(s string) {
+				echo(c, s, 1*time.Second)
+				wg.Done()
+			}(text)
+			timer.Reset(10 * time.Second)
+		case <-timer.C:
+			wg.Wait()
+			c.Close()
+			return
+		case <-abort:
+			wg.Wait()
+			c.Close()
+			return
+		}
 	}
-	wg.Wait()
-	c.Close()
 }
 
 func main() {
