e499f89b99b8802a6ab19b39dba45f633ff96af0
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2018-02-18 15:17:39 +0900

    Copy from ch05/fetch

---
+++ ch08/ex11/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		filename, n, err := fetch(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "title: %v\n", err)
+		}
+		fmt.Fprintf(os.Stdout, "filename: %s\nsize: %d\n", filename, n)
+	}
+}
+func fetch(url string) (filename string, n int64, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", 0, err
+	}
+	defer resp.Body.Close()
+
+	local := path.Base(resp.Request.URL.Path)
+	if local == "/" {
+		local = "index.html"
+	}
+	f, err := os.Create(local)
+	if err != nil {
+		return "", 0, err
+	}
+	n, err = io.Copy(f, resp.Body)
+	if closeErr := f.Close(); err == nil {
+		err = closeErr
+	}
+	return local, n, err
+
+}
