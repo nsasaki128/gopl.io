a5b7576f35ca369dfe657ceae9166b272f07a7f4
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2018-02-22 00:17:09 +0900

    Add implementation of tcp connect

---
+++ ch08/ex02/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+)
+
+var port = flag.Int("port", 8000, "listen port (default 8000)")
+
+func main() {
+	flag.Parse()
+
+	listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		//TODO ここからゴルーチンでメイン処理を呼び出す
+
+		conn.Close()
+	}
+}
