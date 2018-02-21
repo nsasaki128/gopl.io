2be3d4b16cd094c4e35068e28a8f9a6aaef97790
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2018-02-22 00:07:36 +0900

    Add FTP reply code

---
+++ ch08/ex02/codes.go
@@ -0,0 +1,42 @@
+package main
+
+//see https://tools.ietf.org/rfc/rfc640.txt
+const (
+	CommandOk                         = 200
+	CommandSyntaxError                = 500
+	ParamsSyntaxError                 = 501
+	CommandNotImplementedSuperfluous  = 202
+	CommandNotImplemented             = 502
+	BadSequenceCommand                = 503
+	CommandNotImplementedForParameter = 504
+	RestartMarkerReply                = 110
+	SystemStatus                      = 211
+	DirectoryStatus                   = 212
+	FileStatus                        = 213
+	HelpMessage                       = 214
+	ServiceReadyInNMinutes            = 120
+	ServiceReadyForNewUser            = 220
+	ServiceClosingTELNETConnection    = 221
+	ServiceNotAvailable               = 421
+	DataConnectionAlreadyOpen         = 125
+	DataConnectionOpen                = 225
+	CantOpenDataConnection            = 425
+	ClosingDataConnection             = 226
+	ConnectionTrouble                 = 426
+	Entering                          = 227
+	UserLoggedOn                      = 230
+	NotLoggedIn                       = 530
+	UserNameOkay                      = 331
+	NeedAccountForLogin               = 332
+	NeedAccountForStoringFiles        = 532
+	FileStatusOkay                    = 150
+	RequestedFileActionOkey           = 250
+	RequestedFileActionPending        = 350
+	FileUnavailable                   = 450
+	FileUnavailableBusy               = 550
+	LocalErrorInProcessing            = 451
+	InsufficientStorageSpace          = 452
+	ExceededStorageAllocation         = 552
+	FileNameNotAllowed                = 553
+	StartMailInput                    = 354
+)
