e61a277df262da4bbed45e3df9e5d5ae60cf5968
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2018-02-26 21:48:03 +0900

    Add example

---
+++ ch10/jpeg/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/jpeg"
+	_ "image/png" //PNGデコーダーを用意する
+	"io"
+	"os"
+)
+
+func main() {
+	if err := toJPEG(os.Stdin, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "jpeg: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func toJPEG(in io.Reader, out io.Writer) error {
+	img, kind, err := image.Decode(in)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(os.Stderr, "Input format = ", kind)
+	return jpeg.Encode(out, img, &jpeg.Options{Quality: 95})
+
+}
