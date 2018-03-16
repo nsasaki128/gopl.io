631f7769726c4f88b3ebcb42e4e998ca421a7730
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2018-03-17 00:02:39 +0900

    Add zip reader

--- ch10/ex02/archive/zip/reader.go
+++ ch10/ex02/archive/zip/reader.go
@@ -3,6 +3,11 @@ package zip
 import (
 	"os"
 
+	"archive/zip"
+
+	"errors"
+	"io"
+
 	"gopl.io/ch10/ex02/archive"
 )
 
@@ -11,9 +16,42 @@ import (
 //https://blog.freedom-man.com/zip-structure-golang/
 //https://imgur.com/BXuOFqT
 
-//TODO: FIX this function
+type reader struct {
+	r *zip.ReadCloser
+	i int
+}
+
+func (r *reader) Next() (*archive.Header, error) {
+	r.i++
+	if len(r.r.File) <= r.i {
+		r.r.Close()
+		return nil, io.EOF
+	}
+	return &archive.Header{r.r.File[r.i].Name}, nil
+}
+
+func (r *reader) Read(b []byte) (n int, err error) {
+	if r.r == nil {
+		return 0, errors.New("invalid use of zip reader")
+	}
+	f := r.r.File[r.i]
+	reader, err := f.Open()
+	if err != nil {
+		return 0, err
+	}
+	return reader.Read(b)
+}
+
 func read(f *os.File) (archive.Reader, error) {
-	return nil, nil
+	_, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+	r, err := zip.OpenReader(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	return &reader{r: r, i: -1}, nil
 }
 
 func init() {
