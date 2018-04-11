85caafcd33a0efa6f7fec860e99a4041223d8da8
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2018-04-11 21:26:04 +0900

    I misunderstand the problem and therfore I also solved ex08.

---
+++ ch12/ex08/decode_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestDecoder_Decode(t *testing.T) {
+	type Result struct {
+		Name string
+		Comp complex64
+		Fl   float32
+	}
+	var tests = []struct {
+		name  string
+		input string
+		want  interface{}
+	}{
+		{name: "empty", input: `()`, want: Result{}},
+		{name: "include value",
+			input: `
+(
+(Name "hoge") 
+(Comp #C(2.0 3.0)) 
+(Fl 3.14)
+)`, want: Result{Name: "hoge", Comp: complex(2.0, 3.0), Fl: float32(3.14)}},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var got Result
+
+			dec := NewDecoder(strings.NewReader(test.input))
+			err := dec.Decode(&got)
+			if err != nil || !reflect.DeepEqual(got, test.want) {
+				t.Errorf("dec.Decode %#v want %#v err=nil; but actual %#v err=%#v", test.input, test.want, got, err)
+			}
+		})
+	}
+}
