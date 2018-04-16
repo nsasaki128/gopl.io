4914616c9de46ca81211caefee8772d7e8e0e377
Naoki SASAKI <naoki.robert.sasaki@gmail.com>
2018-04-16 23:35:50 +0900

    Solve ex01

---
+++ ch13/ex01/main.go
@@ -0,0 +1,109 @@
+package equal
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+func equal(x, y reflect.Value, seen map[comparison]bool) bool {
+	if !x.IsValid() || !y.IsValid() {
+		return x.IsValid() == y.IsValid()
+	}
+	if x.Type() != y.Type() {
+		return false
+	}
+
+	//
+	if x.CanAddr() && y.CanAddr() {
+		xptr := unsafe.Pointer(x.UnsafeAddr())
+		yptr := unsafe.Pointer(y.UnsafeAddr())
+		if xptr == yptr {
+			return true
+		}
+		c := comparison{xptr, yptr, x.Type()}
+		if seen[c] {
+			return true
+		}
+		seen[c] = true
+	}
+
+	switch x.Kind() {
+	case reflect.Bool:
+		return x.Bool() == y.Bool()
+
+	case reflect.String:
+		return x.String() == y.String()
+
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32,
+		reflect.Int64:
+		return x.Int() == y.Int()
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32,
+		reflect.Uint64, reflect.Uintptr:
+		return x.Uint() == y.Uint()
+
+	case reflect.Float32, reflect.Float64:
+		return EqualFloat(x.Float(), y.Float())
+
+	case reflect.Complex64, reflect.Complex128:
+		return EqualComplex(x.Complex(), y.Complex())
+
+	case reflect.Chan, reflect.UnsafePointer, reflect.Func:
+		return x.Pointer() == y.Pointer()
+
+	case reflect.Ptr, reflect.Interface:
+		return equal(x.Elem(), y.Elem(), seen)
+
+	case reflect.Array, reflect.Slice:
+		if x.Len() != y.Len() {
+			return false
+		}
+		for i := 0; i < x.Len(); i++ {
+			if !equal(x.Index(i), y.Index(i), seen) {
+				return false
+			}
+		}
+		return true
+	case reflect.Struct:
+		for i, n := 0, x.NumField(); i < n; i++ {
+			if !equal(x.Field(i), y.Field(i), seen) {
+				return false
+			}
+		}
+		return true
+
+	case reflect.Map:
+		if x.Len() != y.Len() {
+			return false
+		}
+		for _, k := range x.MapKeys() {
+			if !equal(x.MapIndex(k), y.MapIndex(k), seen) {
+				return false
+			}
+		}
+		return true
+	}
+	panic("unreachable")
+
+}
+
+func Equal(x, y interface{}) bool {
+	seen := make(map[comparison]bool)
+	return equal(reflect.ValueOf(x), reflect.ValueOf(y), seen)
+}
+
+const epsilon = 1.0E-10
+
+func EqualFloat(x, y float64) bool {
+	minX, maxX := x-epsilon, x+epsilon
+	return minX <= y && y <= maxX
+}
+
+func EqualComplex(x, y complex128) bool {
+	return EqualFloat(real(x), real(y)) && EqualFloat(imag(x), imag(y))
+}
+
+type comparison struct {
+	x, y unsafe.Pointer
+	t    reflect.Type
+}
